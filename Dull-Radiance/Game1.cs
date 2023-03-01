dc3a61b28c8a3d87c3d5b4dcd4b30560cfa6af05
Will Forbes <waf1035@rit.edu>
2023-03-01 10:16:12 -0500

    Added Commented Header

--- Dull-Radiance/Game1.cs
+++ Dull-Radiance/Game1.cs
@@ -2,6 +2,12 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+/* Game: Dull Radience
+ * Dev Team: Rice Bowls
+ * Rice in the Bowl: David Gao, Jason Chen, Thaw Thaw, Will Forbes
+ * 
+ */
+
 namespace Dull_Radiance
 {
     public class Game1 : Game
