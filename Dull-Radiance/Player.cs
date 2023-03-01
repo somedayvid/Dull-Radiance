92b980d01644c9ade8848730dad36390827a1054
Will Forbes <waf1035@rit.edu>
2023-03-01 10:31:09 -0500

    Added Basic Player Movement

--- Dull-Radiance/Player.cs
+++ Dull-Radiance/Player.cs
@@ -4,9 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
 namespace Dull_Radiance
 {
+    /// <summary>
+    /// The main player class
+    /// </summary>
     internal class Player
-{
-}
+    {
+        //Variables
+        public KeyboardState KBState;
+
+
+        /// <summary>
+        /// Players Movement using WASD and Arrow Keys
+        /// </summary>
+        public void Movement()
+        {
+            //KBState
+            KBState = Keyboard.GetState();
+
+            //Movement
+            if (KBState.IsKeyDown(Keys.A) || KBState.IsKeyDown(Keys.Left))
+            {
+                //playerRect.X -= 5;
+            }
+            if (KBState.IsKeyDown(Keys.D) || KBState.IsKeyDown(Keys.Right))
+            {
+                //playerRect.X += 5;
+            }
+            if (KBState.IsKeyDown(Keys.W) || KBState.IsKeyDown(Keys.Up))
+            {
+                //playerRect.Y -= 5;
+            }
+            if (KBState.IsKeyDown(Keys.S) || KBState.IsKeyDown(Keys.Down))
+            {
+                //playerRect.Y -= 5;
+            }
+        }
+    }
 }
