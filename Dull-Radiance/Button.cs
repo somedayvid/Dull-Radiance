692ee6172e73c188195e4a5b671554fc615c8130
David Gao <david.04.gao@gmail.com>
2023-03-14 20:09:35 -0400

    BUTTONS ARE SORT OF FUNCTIONAL, DOES NOT IMPLEMENT INTERFACES BC I STUPID.  START MENU HAS BUTTONS THAT HAVE VERY SMALL TEXT. I HOPE YOUR EYES AREN'T TOO BAD. PAIN

--- Dull-Radiance/Button.cs
+++ Dull-Radiance/Button.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +9,125 @@ using System.Threading.Tasks;
 
 namespace Dull_Radiance
 {
+    /// <summary>
+    /// Controls the positioning, clicking, and sizing of buttons
+    /// </summary>
     internal class Button //: ICollideAndDraw
     {
+        //fields
+        private int xPos;
+        private int yPos;
+        private int width;
+        private int height;
+        private Texture2D buttonHover;
+        private Texture2D buttonUnhover;
+        private Texture2D buttonTexture;
+        private Rectangle buttonRect;
+        private int windowHeight;
+        private int windowWidth;
+        private MouseState mState;
+        private int unhoverY;
+        private int hoverY;
+        private SpriteFont font;
 
+        /// <summary>
+        /// Initializes the position of the button and other variables used in determining 
+        /// button position
+        /// </summary>
+        /// <param name="xPos">X position relative to window width</param>
+        /// <param name="yPos">Y position relative to window height</param>
+        /// <param name="buttonHover">What the button looks like when clicked</param>
+        /// <param name="buttonUnhover">What the button looks like when not clicked</param>
+        /// <param name="graphicsDevice">Used in determining width and height of buttons</param>
+        /// <param name="font">Font used for the button text</param>
+        //TODO CLEAN THIS UP ITS DISGUSTING FROM DAVID TO DAVID
+        public Button(int xPos, int yPos, Texture2D buttonHover, Texture2D buttonUnhover, GraphicsDeviceManager graphicsDevice, SpriteFont font)
+        {
+            this.xPos = xPos;
+            this.yPos = yPos;
+            this.buttonHover = buttonHover;
+            this.buttonUnhover = buttonUnhover;
+            this.buttonTexture = buttonUnhover;
+            windowHeight = graphicsDevice.PreferredBackBufferHeight;
+            windowWidth = graphicsDevice.PreferredBackBufferWidth;
+            width = windowWidth/5;
+            height = windowHeight/8;
+            this.hoverY = yPos + windowHeight/72;
+            this.unhoverY = yPos;
+            this.font = font;
+            this.buttonRect = new Rectangle(xPos, unhoverY, width, height);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void ButtonsUpdate(GameTime gameTime)
+        {
+            mState = Mouse.GetState();
+        }
+
+        /// <summary>
+        /// Checks if the button is intersecting with the mouse and has been left clicked
+        /// </summary>
+        /// <returns>True or false depending on mouse position and click</returns>
+        public bool Click()
+        {
+
+            if (ButtonIntersects() && mState.LeftButton == ButtonState.Pressed)
+            {
+                
+                //this.height = windowHeight/9;
+                //this.buttonRect.Y = hoverY;
+                //this.buttonTexture = this.buttonHover;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Checks if the mouse is intersecting with the button
+        /// </summary>
+        /// <returns>True or false depending on mouse position</returns>
+        public bool ButtonIntersects()
+        {
+            if (buttonRect.X <= mState.X && (buttonRect.X + width) >= mState.X
+                && buttonRect.Y <= mState.Y && buttonRect.Y + height >= mState.Y)
+            {
+                this.height = windowHeight/9;
+                this.buttonRect.Y = hoverY;
+                this.buttonTexture = this.buttonHover;
+                return true;
+            }
+            else
+            {
+                this.height = windowHeight/8;
+                this.buttonRect.Y = unhoverY;
+                this.buttonTexture = this.buttonUnhover;
+            }
+                return false;
+        }
+
+        /// <summary>
+        /// Draws the button and the text that accompanies the button
+        /// </summary>
+        /// <param name="sb">Spritebatch to draw</param>
+        /// <param name="buttonText">Text that describes button function</param>
+        public void DrawButton(SpriteBatch sb, string buttonText)
+        {
+            sb.Draw(
+                this.buttonTexture,
+                buttonRect,
+                Color.White);
+
+            sb.DrawString(font,
+                buttonText,
+                new Vector2(this.xPos + width/4, this.yPos + height/2),
+                Color.White);
+        }
 
     }
 }
--- Dull-Radiance/Game1.cs
+++ Dull-Radiance/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 /* Game: Dull Radience
  * Dev Team: Rice Bowls
@@ -43,7 +44,18 @@ namespace Dull_Radiance
         private Texture2D lights;
         private Texture2D hearts;
         private Texture2D deadhearts;
+
+        //menus
         private Texture2D titleScreen;
+
+        //button items
+        private List<Button> buttonList;
+        private Button startButton;
+        private Button quitButton;
+        private Button controlsButton;
+        private Texture2D buttonHoverTexture;
+        private Texture2D buttonUnhoverTexture;
+        private SpriteFont agencyFB;
 
         //Window Numbers
         private int windowHeight;
@@ -67,14 +79,28 @@ namespace Dull_Radiance
             _graphics.IsFullScreen = true;
             _graphics.ApplyChanges();
             base.Initialize();
+
+            buttonList = new List<Button>
+            {
+                startButton,
+                controlsButton,
+                quitButton
+            };
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            titleScreen = Content.Load<Texture2D>("Start_Menu(1)");
+            titleScreen = Content.Load<Texture2D>("StartMenu");
+            buttonUnhoverTexture = Content.Load<Texture2D>("BUTTON_UNHOVER");
+            buttonHoverTexture = Content.Load<Texture2D>("BUTTON_HOVER");
+            agencyFB = Content.Load<SpriteFont>("Agency FB");
             
+
+            startButton = new Button(windowWidth/10, windowHeight/5, buttonHoverTexture, buttonUnhoverTexture, _graphics, agencyFB);
+            controlsButton = new Button(windowWidth/10, windowHeight/3, buttonHoverTexture, buttonUnhoverTexture, _graphics, agencyFB);
+            quitButton = new Button(windowWidth/10, windowHeight/2, buttonHoverTexture, buttonUnhoverTexture, _graphics, agencyFB);
         }
 
         protected override void Update(GameTime gameTime)
@@ -89,6 +115,24 @@ namespace Dull_Radiance
             switch (currentState)
             {
                 case GameState.Title:
+                    foreach(Button button in buttonList)
+                    {
+                        button.ButtonsUpdate(gameTime);
+                    }
+                    if (startButton.Click())
+                    {
+                        currentState = GameState.Game;
+                    }
+                    if(controlsButton.Click())
+                    {
+                        currentState = GameState.Instructions;
+                    }
+                    if (quitButton.Click())
+                    {
+                        System.Threading.Thread.Sleep(1000);
+                        Exit();
+                    }
+                    
                     break;
                 case GameState.Instructions:
                     break;
@@ -123,6 +167,9 @@ namespace Dull_Radiance
                     _spriteBatch.Draw(titleScreen,
                         new Rectangle(0, 0, windowWidth, windowHeight),
                         Color.White);
+                    startButton.DrawButton(_spriteBatch, "Start");
+                    controlsButton.DrawButton(_spriteBatch, "Controls");
+                    quitButton.DrawButton(_spriteBatch, "Quit");
                     break;
                 case GameState.Instructions:
                     break;
